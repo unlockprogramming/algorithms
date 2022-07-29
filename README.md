bae205530d7bbb0c90e5c5350fcc119b24706ac8
Bhuwan Prasad Upadhyay <bhuwan.upadhyay49@gmail.com>
2022-07-29 10:22:10 -0500

    docs: notes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# algorithms
-Algorithms
+# Algorithms
+
+Cruted list and their solution in java for various problems from leetcode.
+
+## LeetCode
+
+- [ ] [Two Sum]()
---
+++ src/main/java/com/unlockprogramming/algorithms/_001_TwoSum.java
@@ -0,0 +1,16 @@
+package com.unlockprogramming.algorithms;
+
+/**
+ * 
+ * Leetcode: https://leetcode.com/problems/two-sum/
+ * 
+ * @author Bhuwan Prasad Upadhyay
+ */
+public class _001_TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        
+    }
+
+    
+}
