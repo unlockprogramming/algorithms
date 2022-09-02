bb8bfafcdb4e9490e2d8ca921d2f64615d9c3e87
Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>
2022-09-02 06:26:55 -0500

    Develop (#1)
    * docs: notes
    
    * #002 https://leetcode.com/problems/maximum-average-subarray-i/
    
    * docs: notes
    
    * docs: notes
    
    * docs: notes

---
+++ src/main/java/com/unlockprogramming/algorithms/_002_find_maximum_average_of_sub_arrays/BruteForce.java
@@ -0,0 +1,39 @@
+package com.unlockprogramming.algorithms._002_find_maximum_average_of_sub_arrays;
+
+/**
+ * <a href="https://leetcode.com/problems/maximum-average-subarray-i/">Question</a>
+ *
+ * @author Bhuwan Prasad Upadhyay
+ */
+class BruteForce {
+
+    public double findMaxAverage(int[] nums, int k) {
+
+        // To support any numbers (positives, zero, negatives)
+        double maxAverage = Double.NEGATIVE_INFINITY;
+
+        /*
+            MATH: to find number of possible contiguous sub arrays
+            - number of sub arrays = total number of elements - subarray size + 1
+         */
+        for (int i = 0; i < nums.length - k + 1; i++) {
+
+            double currentSum = 0;
+
+            // Calculate sum of elements in current subarray
+            for (int j = i; j < i + k; j++) {
+                currentSum += nums[j];
+            }
+
+            // Calculate average of elements in current subarray
+            double currentAverage = currentSum / k;
+
+            // Replace max average if current average is maximum
+            maxAverage = Math.max(maxAverage, currentAverage);
+
+        }
+
+        return maxAverage;
+    }
+
+}
---
+++ src/main/java/com/unlockprogramming/algorithms/_002_find_maximum_average_of_sub_arrays/Optimized.java
@@ -0,0 +1,44 @@
+package com.unlockprogramming.algorithms._002_find_maximum_average_of_sub_arrays;
+
+/**
+ * <a href="https://leetcode.com/problems/maximum-average-subarray-i/">Question</a>
+ *
+ * @author Bhuwan Prasad Upadhyay
+ */
+class Optimized {
+
+    public double findMaxAverage(int[] nums, int k) {
+
+        // To support any numbers (positives, zero, negatives)
+        double maxAverage = Double.NEGATIVE_INFINITY;
+        double sum = 0;
+        int start = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+
+            //increase sum by adding the element which is included in sliding window
+            sum += nums[end];
+
+            //move sliding window forward, only if we have hit the required sliding window size of k
+            if (end >= k - 1) {
+
+                // Calculate average of elements in current sliding window
+                double currentAverage = sum / k;
+
+                // Replace max average if current average is maximum
+                maxAverage = Math.max(maxAverage, currentAverage);
+
+                //decrease sum by subtracting the element which is going out from sliding window
+                sum -= nums[start];
+
+                // move the sliding window forward
+                start++;
+
+            }
+
+        }
+
+        return maxAverage;
+    }
+
+}
